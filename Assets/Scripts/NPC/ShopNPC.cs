289e76f5226bf05d7d048692a1ee304f3865c5e7
kimdaeyeon <extract08@naver.com>
2023-11-22 18:24:20 +0900

    store step1

---
+++ Assets/Scripts/NPC/ShopNPC.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShopNPC : MonoBehaviour
+{
+    public GameObject uiGruop;
+    public Animator anim;
+
+    Player enterPlayer;
+
+    public void Enter()
+    {
+        Debug.Log("Enter");
+        uiGruop.SetActive(true);
+    }
+
+    public void Exit()
+    {
+        Debug.Log("Exit");
+        uiGruop.SetActive(false);
+    }
+
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            Enter();
+        }
+    }
+
+    public void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Exit();
+        }
+    }
+}
