6d15c6d0d4188207000cdfaecb0f31c2d0140b38
kimdaeyeon <extract08@naver.com>
2023-12-06 00:22:46 +0900

    damage text

--- Assets/Scripts/Enemy/EnemyHealth.cs
+++ Assets/Scripts/Enemy/EnemyHealth.cs
@@ -9,6 +9,9 @@ public class EnemyHealth : Enemy
     public float maxHP = 10.0f;
     EnemyBase eb;
 
+    public GameObject hudDamageText;
+    public Transform hudPos;
+
     void Start()
     {
         eb = GetComponent<EnemyBase>();
@@ -21,20 +24,29 @@ public class EnemyHealth : Enemy
 
     public void GetDamage(float damage)
     {
-        if(curHP > 0) { 
-        curHP -= damage;
+        if(curHP > 0) 
+        { 
+            curHP -= damage;
+            DamageText(damage);
+            if (IsDeath()) 
+            {
+                curHP = 0.0f;
+                isDie = true;
+            }
 
-        if (IsDeath()) {
-            curHP = 0.0f;
-            isDie = true;
-        }
-
-        StartCoroutine(eb.OnDamage());
+            StartCoroutine(eb.OnDamage());
         }
         else
         {
             curHP = 0.0f;
         }
+    }
+
+    void DamageText(float damage)
+    {
+        GameObject hudText = Instantiate(hudDamageText); // 생성할 텍스트 오브젝트
+        hudText.transform.position = hudPos.position; // 표시될 위치
+        hudText.GetComponent<DamageText>().damage = damage; // 데미지 전달
     }
 
     public void RecoveryHP(float hp)
